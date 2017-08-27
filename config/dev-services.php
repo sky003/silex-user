07e0f8ec5faf1736fcce63a97774e744410326fa
Anton Pelykh <anton.pelykh.dev@gmail.com>
2017-08-27 14:55:51 +0300

    Logger configuration.

--- config/dev-services.php
+++ config/dev-services.php
@@ -5,4 +5,31 @@
  * @var \Auth\Application $app
  */
 
+use Gelf\Publisher;
+use Gelf\Transport\UdpTransport;
+use Monolog\Formatter\GelfMessageFormatter;
+use Monolog\Handler\GelfHandler;
+use Silex\Provider\MonologServiceProvider;
+
 require 'dev.php';
+
+$app->register(new MonologServiceProvider(), [
+    'monolog.use_error_handler' => false,
+]);
+$app->extend('monolog', function (Monolog\Logger $logger) use ($app) {
+    // TODO: Wrap this into service provider.
+    $handler = new GelfHandler(
+        new Publisher(
+            new UdpTransport(
+                $app['graylog.options']['host'],
+                $app['graylog.options']['port'],
+                UdpTransport::CHUNK_SIZE_LAN
+            )
+        )
+    );
+    $handler->setFormatter(new GelfMessageFormatter());
+
+    $logger->pushHandler($handler);
+
+    return $logger;
+});
--- config/dev.php
+++ config/dev.php
@@ -4,3 +4,27 @@
  *
  * @var \Auth\Application $app
  */
+
+$app['debug'] = true;
+$app['cache.dir'] = dirname(__DIR__).'/var/cache';
+$app['logger.dir'] = dirname(__DIR__).'/var/log';
+
+$app['graylog.options'] = [
+    'host' => getenv('GRAYLOG_HOST'),
+    'port' => getenv('GRAYLOG_PORT'),
+];
+
+$app['db.postgres_options'] = [
+    'driver' => 'pdo_pgsql',
+    'dbname' => getenv('POSTGRES_DB'),
+    'user' => getenv('POSTGRES_USER'),
+    'password' => getenv('POSTGRES_PASSWORD'),
+    'host' => getenv('POSTGRES_HOST'),
+    'port' => getenv('POSTGRES_PORT'),
+];
+
+$app['redis.options'] = [
+    'host' => getenv('REDIS_HOST'),
+    'port' => getenv('REDIS_PORT'),
+    'db' => getenv('REDIS_DB'),
+];
